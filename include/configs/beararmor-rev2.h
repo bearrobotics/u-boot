241b0380e46d962d75766cb01909e6bcc68c902c
Kitae Kim <kitaekim27@gmail.com>
2025-08-07 17:14:48 +0900

    Support BearArmor rev2 board with measured boot
    Add support for the BearArmor revision 2 board, which uses a discrete
    TPM and enables measured boot.
    
    Reserve 64 KiB at 0x28000000 for the TPM event log. Load the kernel at
    0x02000000, the FDT at 0x12000000, and the initramfs at 0x12180000. The
    initramfs is expected to remain below 256 MiB.
    
    The devicetree does not yet describe the GPIO connected to the TPM reset
    pin (reset-gpios). As a result, warm boots do not reset PCRs and
    measurements are extended on top of values from previous boots. This is
    not intended and will be fixed once the TPM reset GPIO is identified.

---
+++ include/configs/beararmor-rev2.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+#ifndef __BEARARMOR_REV2_H
+#define __BEARARMOR_REV2_H
+
+#include <configs/rk3588_common.h>
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
+
+#endif
